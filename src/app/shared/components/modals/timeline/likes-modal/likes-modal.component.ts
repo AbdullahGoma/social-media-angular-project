24d850bc46089a9c1b6fd838b49f4b9347cad4bb
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2025-06-18 22:10:41 +0300

    Add Comments and Likes Modals

---
+++ src/app/shared/components/modals/timeline/likes-modal/likes-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-likes-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './likes-modal.component.html',
+  styleUrl: './likes-modal.component.css'
+})
+export class LikesModalComponent {
+
+}
