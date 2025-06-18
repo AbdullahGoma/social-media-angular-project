231ac1e9b4b46b08a21a04649cd7d1a651be83ea
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2025-06-18 21:12:44 +0300

    Generate chat components

---
+++ src/app/shared/components/modals/chat/chat-window-modal/chat-window-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-window-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './chat-window-modal.component.html',
+  styleUrl: './chat-window-modal.component.css'
+})
+export class ChatWindowModalComponent {
+
+}
