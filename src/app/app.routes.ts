75787303756f3c2f2522b7ce69c0b15b48f2d987
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2025-06-18 22:01:08 +0300

    Add Routes Configuration with Lazy Loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const APP_ROUTES: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./features/layout/layout.routes').then((m) => m.LAYOUT_ROUTES),
+    children: [
+      { path: '', redirectTo: 'timeline', pathMatch: 'full' },
+      {
+        path: 'timeline',
+        loadChildren: () =>
+          import('./features/timeline/timeline.routes').then(
+            (m) => m.TIMELINE_ROUTES
+          ),
+      },
+      {
+        path: 'profile/:id',
+        loadChildren: () =>
+          import('./features/profile/profile.routes').then(
+            (m) => m.PROFILE_ROUTES
+          ),
+      },
+      {
+        path: 'settings',
+        loadChildren: () =>
+          import('./features/settings/settings.routes').then(
+            (m) => m.SETTINGS_ROUTES
+          ),
+      },
+    ],
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./features/auth/auth.routes').then((m) => m.AUTH_ROUTES),
+  },
+  { path: '**', redirectTo: 'timeline' },
+];
