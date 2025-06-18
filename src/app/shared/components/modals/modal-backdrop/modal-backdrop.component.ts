a1c15a758d9fe34b94d0fbe3f7acf6cba82218e0
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2025-06-18 21:08:25 +0300

    Generate shared modal container and service

---
+++ src/app/shared/components/modals/modal-backdrop/modal-backdrop.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-backdrop',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-backdrop.component.html',
+  styleUrl: './modal-backdrop.component.css'
+})
+export class ModalBackdropComponent {
+
+}
---
+++ src/app/shared/components/modals/modal.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appModal]',
+  standalone: true
+})
+export class ModalDirective {
+
+  constructor() { }
+
+}
