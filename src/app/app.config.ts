4892f387c1cf167a59e76ea74cf7f7ce036a5ab0
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2025-06-18 22:33:20 +0300

    Make the App Zoneless and Fix Duplication Routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { APP_ROUTES } from './app.routes';
 
@@ -6,5 +6,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(APP_ROUTES),
+    [provideExperimentalZonelessChangeDetection()]
   ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,31 +4,7 @@ export const APP_ROUTES: Routes = [
   {
     path: '',
     loadChildren: () =>
-      import('./features/layout/layout.routes').then((m) => m.LAYOUT_ROUTES),
-    children: [
-      { path: '', redirectTo: 'timeline', pathMatch: 'full' },
-      {
-        path: 'timeline',
-        loadChildren: () =>
-          import('./features/timeline/timeline.routes').then(
-            (m) => m.TIMELINE_ROUTES
-          ),
-      },
-      {
-        path: 'profile/:id',
-        loadChildren: () =>
-          import('./features/profile/profile.routes').then(
-            (m) => m.PROFILE_ROUTES
-          ),
-      },
-      {
-        path: 'settings',
-        loadChildren: () =>
-          import('./features/settings/settings.routes').then(
-            (m) => m.SETTINGS_ROUTES
-          ),
-      },
-    ],
+      import('./features/layout/layout.routes').then((m) => m.LAYOUT_ROUTES)
   },
   {
     path: 'auth',
