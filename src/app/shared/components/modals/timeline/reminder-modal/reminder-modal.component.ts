fa2e5e711ef515a6917effa5c10b3d70ed7f3ad9
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2025-06-19 17:47:25 +0300

    Add Reminder Modal and it's style and fix errors with right and left sidebars

---
+++ src/app/shared/components/modals/timeline/reminder-modal/reminder-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reminder-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './reminder-modal.component.html',
+  styleUrl: './reminder-modal.component.css'
+})
+export class ReminderModalComponent {
+
+}
