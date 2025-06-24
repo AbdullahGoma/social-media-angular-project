7448627cd046bfa0ba2ca95b9167b1a937efd16c
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2025-06-24 18:07:23 +0300

    Add No Auth Guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './features/landing/landing.component';
+import { noAuthGuard } from './core/guards/no-auth.guard';
 
 export const APP_ROUTES: Routes = [
+  // {
+  //   path: '',
+  //   component: LandingComponent,
+  //   canActivate: [noAuthGuard],
+  // },
   {
     path: '',
-    component: LandingComponent,
-  },
-  {
-    path: 'app',
     loadChildren: () =>
       import('./features/layout/layout.routes').then((m) => m.LAYOUT_ROUTES),
   },
---
+++ src/app/core/guards/no-auth.guard.ts
@@ -0,0 +1,12 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+export const noAuthGuard: CanActivateFn = (route, state) => {
+  // const authService = inject(AuthService);
+  const router = inject(Router);
+
+  // if (authService.isLoggedIn()) {
+    return router.parseUrl('/app/timeline');
+  // }
+  // return true;
+};
